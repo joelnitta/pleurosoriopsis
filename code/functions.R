cf5a7c337fc9e0ee9fc0465bc5b456057a5962b9
Joel Nitta <joelnitta@gmail.com>
2019-01-31 12:20:49 -0500

    Move functions to separate script, restructure analysis flow to read-in data, then plot four figures

---
+++ code/functions.R
@@ -0,0 +1,161 @@
+#' Run a paired t-test comparing a microclimatic variable between
+#' two sites
+#'
+#' @param var String; Name of the variable
+#' @param paired_data Dataframe including measured variable at two sites
+#' @param site1 String; Name of first site
+#' @param site2 String; Name of second site
+#'
+#' @return Numeric; p-value for null hypothesis that the difference between
+#' the two means is zero.
+#'
+#' @examples
+#' run_t_test("rh_min", paired_microclimate, "okutama", "uratakao")
+run_t_test <- function (var, paired_data, site1 = "okutama", site2 = "uratakao") {
+  
+  var <- rlang::sym(var)
+  
+  test_data <-
+    paired_data %>%
+    select(date, !!var, site) %>%
+    spread(site, !!var) %>%
+    drop_na
+  
+  t.test(test_data[[site1]], test_data[[site2]], mu = 0, paired = TRUE) %>%
+    tidy() %>%
+    mutate(p.value = round(p.value, 3)) %>%
+    pull(p.value)
+  
+}
+
+#' Make ridge plot showing distribution of microclimatic for two sites
+#' and show p-value if they are significantly different.
+#'
+#' @param var String; Name of the variable
+#' @param paired_data Dataframe including measured variable at two sites
+#' @param site1 String; Name of first site
+#' @param site2 String; Name of second site
+#'
+#' @return GGplot object
+#'
+#' @examples
+#' plot_ridges("rh_min", "site", paired_microclimate, x_min = 0, x_max = 100)
+
+plot_ridges <- function (x_var, y_var, data, x_min = NULL, x_max = NULL) {
+  
+  # p.value <- run_t_test(var, paired_data, site1, site2)
+  
+  # x_var <- rlang::sym(x_var)
+  
+  plot <- ggplot(data, aes_string(x = x_var, y = y_var, fill = y_var)) +
+    geom_density_ridges() +
+    theme(
+      legend.position = "none",
+      axis.text.y = element_blank(),
+      axis.title.y = element_blank(),
+      axis.ticks.y = element_blank(),
+      strip.background = element_blank()
+    )
+  
+  if(!is.null(x_min) & !is.null(x_max)) {
+    plot <- plot +
+      xlim(c(x_min, x_max))
+  }
+  
+  plot
+  
+}
+
+#' Function to run lm taking strings as variable names. 
+#' 
+#' Modified from
+#' http://stat545.com/block025_lm-poly.html#use-our-lm-wrapper-with-broom
+#'
+#' @param x_var String, name of x variable (must be name of column in data)
+#' @param y_var String, name of y variable (must be name of column in data)
+#' @param data Dataframe
+#' @param ... Not used by this function but needed so this can be used in pmap
+#' with a dataframe including other non-argument columns.
+make_lm <- function(x_var, y_var, data, ...) {
+  x_var <- rlang::parse_expr(x_var)
+  y_var <- rlang::parse_expr(y_var)
+  lm_formula <- substitute(y_var ~ x_var)
+  eval(lm(lm_formula, data = data))
+}
+
+#' Make a quick scatter plot with a linear regression
+#' 
+#' See these posts on how to get the formula in the upper RHC
+#' 
+#' https://stackoverflow.com/questions/7549694/adding-regression-line-equation-and-r2-on-graph
+#' https://github.com/tidyverse/ggplot2/issues/1244
+#'
+#' @param x_var Name of x variable
+#' @param y_var Name of y data
+#' @param x_lab Label for x variable
+#' @param y_lab Label for y variable
+#' @param plot_data Data frame for plotting including x_var and y_var
+#' @param model_data Data frame of results of linear model fit. Regression
+#' line will be added only for models with p value < 0.05. 
+#' @param no_legend Logical; should the legend be surpressed?
+#'
+#' @return Composite plot
+#' @examples
+#' quick_plot_with_stats(
+#' "temp", "length", 
+#' "Temp (C)", "Length (um)", 
+#' combined_monthly_morph, model_results,
+#' no_legend = FALSE)
+
+quick_plot_with_stats <- function (x_var, y_var, x_lab, y_lab, plot_data, model_data, no_legend = TRUE, cols) {
+  
+  x_filter <- x_var
+  y_filter <- y_var
+  
+  filtered_model_data <- filter(
+    model_data, 
+    x_var == x_filter, 
+    y_var == y_filter
+  )
+  p.value <- pull(filtered_model_data, p.value) %>%
+    signif(2)
+  r.squared <- pull(filtered_model_data, r.squared) %>%
+    signif(2)
+  
+  x_var <- rlang::parse_expr(x_var)
+  y_var <- rlang::parse_expr(y_var)
+  
+  plot <- ggplot(plot_data, aes(x = !!x_var, y = !!y_var)) +
+    # color by month, which is an ordered factor 1-12
+    geom_point(aes(color = month)) +
+    labs(x = x_lab,
+         y = y_lab) +
+    # start at a cool color for jan (teal-blue)
+    scale_colour_hue(h.start = 180)
+  
+  if(isTRUE(no_legend)) {
+    plot <- plot + theme(legend.position="none")
+  }
+  
+  if(p.value < 0.05) {
+    
+    eq <- substitute(italic(r)^2~"="~r2*","~~italic(p)~"="~pval, 
+                     list(r2 = r.squared,
+                          pval = p.value)) %>% 
+      as.expression %>%
+      as.character
+    
+    plot <- plot +
+      geom_line(
+        data = model_data %>% 
+          filter(x_var == x_filter, y_var == y_filter) %>% 
+          pull(fits) %>%
+          first,
+        aes(y = `.fitted`)
+      ) +
+      annotate("text", Inf, Inf, label = eq, hjust = 1, vjust = 1, parse = TRUE)
+  }
+  
+  plot
+  
+}
--- code/morph_clim_analysis.R
+++ code/morph_clim_analysis.R
@@ -12,6 +12,12 @@ library(readxl)
 library(lubridate)
 library(cowplot)
 library(broom)
+# library(viridis)
+library(ggridges)
+
+source("code/functions.R")
+
+# Load data ----
 
 #' Read in raw microclimate data. 
 #' 
@@ -87,30 +93,44 @@ takao_microclimate <- takao_microclimate_raw %>%
   ) %>%
   select(-pm)
 
-#' In order to join the data, need to use mean daily values since
-#' the times are slightly different.
+#' In order to compare data across sites, need to use daily values since
+#' the times are slightly different. Calculate daily min, mean,
+#' max, sd, and total values. We won't use all of these but it's
+#' easier to do them all at once using summarize_at(). Also add season.
+
 daily_microclimate <-
   bind_rows(
     mutate(okutama_microclimate, site = "okutama"),
     mutate(takao_microclimate, site = "uratakao")
   ) %>%
+  filter(complete.cases(.)) %>% # no missing values, so don't need na.rm
   group_by(date, site) %>%
   summarize_at(
     c("par", "temp", "rh"),
-    mean,
-    na.rm = TRUE
+    funs(
+      mean = mean,
+      max = max,
+      min = min,
+      sd = sd,
+      total = sum
+  )) %>%
+  ungroup %>%
+  mutate(
+    month = month(date),
+    season = case_when(
+      month >= 1 & month < 3 ~ "winter",
+      month >= 3 & month < 6 ~ "spring",
+      month >= 6 & month < 9 ~ "summer",
+      month >= 9 & month < 12 ~ "fall",
+      month == 12 ~ "winter"
+    )
   )
 
 daily_microclimate
 
-#' Quick visualization
-daily_microclimate %>%
-  gather(var, value, -date, -site) %>%
-  ggplot(aes(x = date, y = value, color = site)) +
-  geom_line(alpha = 0.5) +
-  facet_wrap(~var, scales = "free")
-
-#' ## Cover data
+#' ## Morphology data
+#' 
+#' ### Cover data
 #'
 #' Cover data was measured for four plots at a single site. 
 #' Since the values for each plots is the number of filled squares, we can 
@@ -136,7 +156,7 @@ cover <- cover_raw %>%
 
 cover
 
-#' ## Gemmae
+#' ### Gemmae
 #'
 #' Read in pre-processed data (need to get raw data from Ebihara)
 
@@ -153,7 +173,7 @@ gemmae_length_raw <- read_xlsx(
   range = "E2:G62")
 
 gemmae_length_raw
-  
+
 #' Tidy data. "month" column originally includes the year only for March 
 #' the first time, then January.
 #' 
@@ -194,27 +214,163 @@ gemmae_data <- full_join(gemmae_count, gemmae_length) %>%
 
 gemmae_data
 
-#' ## Relationship between morphology and mean monthly climate 
+# Fig 1: Change in microclimate over time -----
+daily_microclimate %>%
+  select(-season, -month) %>%
+  gather(var, value, -date, -site) %>% 
+  ggplot(aes(x = date, y = value, color = site)) +
+  geom_line(alpha = 0.5) +
+  facet_wrap(~var, scales = "free")
+
+# Fig 2: Change in morphology over time ----
+
+# Fig 3: Compare microclimate between sites ----
+
+#' For comparing microclimate between sites, subset the data to days only including
+#' means from both sites.
+both_days <-
+  daily_microclimate %>%
+  select(date, site, temp_mean) %>%
+  spread(site, temp_mean) %>%
+  drop_na() %>%
+  pull(date)
+
+paired_microclimate <- daily_microclimate %>%
+  filter(date %in% both_days)
+
+# Make plot including t-test on three microclimatic variables.
+# Final plot includes multiple subplots. Each is a ridge plot showing 
+# the distribution of one of the variables between the two sites, with the
+# results of a paired t-test showing if they are different or not.
+
+selected_vars <- c("rh_min", "par_total", "temp_mean")
+
+# For looping across plots, make cross-product dataframe of all seasons and
+# selected variables.
+var_grid <-
+  list(
+    var = selected_vars,
+    season = c("winter", "spring", "summer", "fall")
+  ) %>% cross_df()
+
+# Need to manually set x ranges to keep x-axis scale the same across plots
+x_ranges <- tibble(var = selected_vars) %>%
+  mutate(
+    min = map_dbl(var, ~ pull(paired_microclimate, .) %>% min ),
+    max = map_dbl(var, ~ pull(paired_microclimate, .) %>% max )
+  )
+
+# For looping across plots, make a nested dataframe by season.
+nested_paired_microclimate <-
+paired_microclimate %>%
+  select(date, site, season, selected_vars) %>%
+  group_by(season) %>%
+  nest %>%
+  right_join(var_grid)
+
+# Make plots by mapping across nested datasets.
+plot_results <-
+  nested_paired_microclimate %>%
+  right_join(x_ranges) %>%
+  mutate(min = min - 0.05 * min,
+         max = max + 0.05 * max,
+         plot = pmap(list(var, data, x_min = min, x_max = max), 
+                     plot_ridges, 
+                     y_var = "site"),
+         pval = map2(var, data, run_t_test),
+         pval = case_when(
+           pval < 0.0001 ~ "***",
+           pval < 0.001 ~ "**",
+           pval < 0.05 ~ "*",
+           TRUE ~ ""
+         ),
+         plot = map2(
+           plot, pval, 
+           ~ .x + annotate("text", Inf, Inf, 
+                           label = .y, hjust = 1, vjust = 1))
+         
+         # If needed, add annotation for season to make sure subplots
+         # are positioned correctly
+         
+         # ,plot = map2(
+         #   plot, season,
+         #   ~ .x + annotate("text", -Inf, Inf,
+         #                   label = .y, hjust = -1, vjust = 1)),
+         # season = as_factor(season),
+         # season = fct_relevel(season, "winter", "spring", "summer", "fall")
+         
+  ) %>% 
+  arrange(season, var)
+
+# Manually tweak some subplots to remove redundant labels, etc.
+plot_results$plot[1:9] <- map(plot_results$plot[1:9], 
+                             ~ . + theme(axis.title.x = element_blank()) )
+
+plot_results$plot[[10]] <- plot_results$plot[[10]] +
+  labs(x = expression("PAR (mmol per sq. cm)"))
+
+plot_results$plot[[11]] <- plot_results$plot[[11]] +
+  labs(x = expression("Rel. Humidity (%)"))
+
+plot_results$plot[[12]] <- plot_results$plot[[12]] +
+  labs(x = expression("Temp. (°C)"))
+
+# Want to add y axis labels for season but this is not working.
+plot_results$plot[[1]] <- plot_results$plot[[1]] +
+  labs(y = "Winter")
+
+plot_results$plot[[4]] <- plot_results$plot[[4]] +
+  labs(y = "Spring")
+
+plot_results$plot[[7]] <- plot_results$plot[[7]] +
+  labs(y = "Summer")
+
+plot_results$plot[[10]] <- plot_results$plot[[10]] +
+  labs(y = "Fall")
+
+main_plot <- plot_grid(plotlist = plot_results$plot, ncol = 3, align = "hv")
+
+legend <- paired_microclimate %>%
+  ggplot(aes(x = temp_mean, y = site, fill = site)) +
+  geom_density_ridges() +
+  theme(legend.position = "bottom")
+legend <- get_legend(legend)
+
+combined_plot <- plot_grid(main_plot, legend, ncol = 1, rel_heights = c(1, .1))
+
+ggplot2::ggsave(plot = combined_plot, filename = "results/ridge_plots.pdf", height = 8, width = 6)
+
+# Fig 4: Relationship between morphology and mean monthly climate ----
 #' 
 #' Run in a loop and plot each combination of climate vars as indep var
 #' and morphology as dep var.
 #' 
-#' Need to calculate mean monthly climate values,
-#' since morphology was measured once per month.
+#' Calculate monthly means of the three selected variables (daily min RH, daily total PAR, daily
+#' mean temp)
+
 okutama_monthly <-
   okutama_microclimate %>%
+  filter(complete.cases(.)) %>% # no missing values, so don't need na.rm
+  group_by(date) %>%
+  summarize_at(
+    c("par", "temp", "rh"),
+    funs(
+      mean = mean,
+      max = max,
+      min = min,
+      sd = sd,
+      total = sum
+    )) %>%
+  ungroup %>%
   mutate(
-    month = month(date_time),
-    year = year(date_time),
+    month = month(date),
+    year = year(date),
     month_year = paste(year, month, sep = "-")) %>%
   group_by(month_year) %>%
   summarize_at(
-    c("par", "temp", "rh"),
-    mean,
-    na.rm = TRUE
+    selected_vars,
+    mean
   )
-
-okutama_monthly
 
 combined_morph <- 
 full_join(
@@ -229,34 +385,18 @@ combined_monthly_morph <- left_join(combined_morph, okutama_monthly) %>%
 combined_monthly_morph
 
 #' ## Fit models
-#'
-#' Function to run lm taking strings as variable names. 
-#' 
-#' Modified from
-#' http://stat545.com/block025_lm-poly.html#use-our-lm-wrapper-with-broom
-#'
-#' @param x_var String, name of x variable (must be name of column in data)
-#' @param y_var String, name of y variable (must be name of column in data)
-#' @param data Dataframe
-#' @param ... Not used by this function but needed so this can be used in pmap
-#' with a dataframe including other non-argument columns.
-make_lm <- function(x_var, y_var, data, ...) {
-  x_var <- rlang::parse_expr(x_var)
-  y_var <- rlang::parse_expr(y_var)
-  lm_formula <- substitute(y_var ~ x_var)
-  eval(lm(lm_formula, data = data))
-}
 
 #' Loop through variables and make a model for each combination.
 #' Note that since var_grid includes additional columns that aren't used
-#' by the model function (y_lab and x_lab),
+#' by the model function (y_lab and x_lab), need to include ... in 
+#' the make_lm() function arguments.
 
 #' Make dataframe of arguments to feed to the lm function. Includes all
 #' combination of dependent (y) and independent (x) variables, and the
 #' axis names to use for plotting.
 var_grid <-
   list(
-    x_var = c("rh", "par", "temp"),
+    x_var = selected_vars,
     y_var = c("length", "number", "total_cover")
   ) %>% cross_df() %>%
   mutate(
@@ -266,15 +406,15 @@ var_grid <-
       y_var == "total_cover" ~ "Total cover (sq. cm)" 
     ),
     x_lab = case_when(
-      x_var == "rh" ~ "Rel. Humidity (%)", 
-      x_var == "par" ~ "PAR (mmol per sq. cm)", 
-      x_var == "temp" ~ "Temp. (°C)" 
+      str_detect(x_var, "rh") ~ "Rel. Humidity (%)", 
+      str_detect(x_var, "par") ~ "PAR (mmol per sq. cm)", 
+      str_detect(x_var, "temp") ~ "Temp. (°C)" 
     )
   )
 
 var_grid
 
-#' Make tibble of data sets. "data" column includes tibbles with
+#' Make tibble of data sets for looping. "data" column includes tibbles with
 #'  x var, y var, month, and year for each set of variables.
 model_data_sets <- pmap(
   var_grid, ~select(combined_monthly_morph, .x, .y, month, year) %>% drop_na)
@@ -301,84 +441,6 @@ model_results <-
 model_results
 
 #' ## Plotting
-#'
-
-#' Make a quick scatter plot with a linear regression
-#' 
-#' See these posts on how to get the formula in the upper RHC
-#' 
-#' https://stackoverflow.com/questions/7549694/adding-regression-line-equation-and-r2-on-graph
-#' https://github.com/tidyverse/ggplot2/issues/1244
-#'
-#' @param x_var Name of x variable
-#' @param y_var Name of y data
-#' @param x_lab Label for x variable
-#' @param y_lab Label for y variable
-#' @param plot_data Data frame for plotting including x_var and y_var
-#' @param model_data Data frame of results of linear model fit. Regression
-#' line will be added only for models with p value < 0.05. 
-#' @param no_legend Logical; should the legend be surpressed?
-#'
-#' @return Composite plot
-#' @examples
-#' quick_plot_with_stats(
-#' "temp", "length", 
-#' "Temp (C)", "Length (um)", 
-#' combined_monthly_morph, model_results,
-#' no_legend = FALSE)
-
-quick_plot_with_stats <- function (x_var, y_var, x_lab, y_lab, plot_data, model_data, no_legend = TRUE, cols) {
-
-  x_filter <- x_var
-  y_filter <- y_var
-  
-  filtered_model_data <- filter(
-    model_data, 
-    x_var == x_filter, 
-    y_var == y_filter
-    )
-  p.value <- pull(filtered_model_data, p.value) %>%
-    signif(2)
-  r.squared <- pull(filtered_model_data, r.squared) %>%
-    signif(2)
-  
-  x_var <- rlang::parse_expr(x_var)
-  y_var <- rlang::parse_expr(y_var)
-  
- plot <- ggplot(plot_data, aes(x = !!x_var, y = !!y_var)) +
-   # color by month, which is an ordered factor 1-12
-   geom_point(aes(color = month)) +
-   labs(x = x_lab,
-        y = y_lab) +
-   # start at a cool color for jan (teal-blue)
-   scale_colour_hue(h.start = 180)
- 
- if(isTRUE(no_legend)) {
-   plot <- plot + theme(legend.position="none")
- }
- 
- if(p.value < 0.05) {
-   
-   eq <- substitute(italic(r)^2~"="~r2*","~~italic(p)~"="~pval, 
-                    list(r2 = r.squared,
-                         pval = p.value)) %>% 
-     as.expression %>%
-     as.character
-  
-   plot <- plot +
-     geom_line(
-       data = model_data %>% 
-         filter(x_var == x_filter, y_var == y_filter) %>% 
-         pull(fits) %>%
-         first,
-       aes(y = `.fitted`)
-     ) +
-     annotate("text", Inf, Inf, label = eq, hjust = 1, vjust = 1, parse = TRUE)
- }
- 
- plot
- 
-}
 
 #' Loop through all combination of dep and indep variables and their models,
 #' and make plots for each.
@@ -390,8 +452,8 @@ subplots <-
 
 #' Extract a legend to add to the combined plot.
 legend <- quick_plot_with_stats(
-  "rh", "length", 
-  "rh", "length", 
+  "rh_min", "length", 
+  "minimum rh", "length", 
   combined_monthly_morph, model_results) + 
   labs(color = "Month") +
   theme(legend.position="bottom")
@@ -416,10 +478,11 @@ main_plot <- plot_grid(plotlist = subplots, align = "hv")
 # Use NULL plots to center legend (need to have same number of columns).
 legend_plot <- plot_grid(NULL, legend, NULL, nrow = 1)
 
-plot_grid(main_plot, legend_plot, ncol = 1, rel_heights = c(1, .2))
+okutama_model_plot <- plot_grid(main_plot, legend_plot, ncol = 1, rel_heights = c(1, .2))
 
 # Save plot
-ggsave("results/Pleurosoriopsis_combined_plot.pdf", width = 7, height = 9)
+ggsave(plot = okutama_model_plot,
+       filename = "results/Pleurosoriopsis_combined_plot.pdf", width = 7, height = 9)
 
 #'
 # Render this script as a report
